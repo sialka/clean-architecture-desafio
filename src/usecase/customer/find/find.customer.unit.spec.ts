a21391abdd6a12ac54e20ec972b2874d9bad61ca
Sidnei Filho <sialkas@gmail.com>
2024-07-09 12:55:38 -0300

    feat: :zap: update

--- src/usecase/customer/find/find.customer.unit.spec.ts
+++ src/usecase/customer/find/find.customer.unit.spec.ts
@@ -1,6 +1,3 @@
-import { Sequelize } from "sequelize-typescript";
-import CustomerModel from "../../../infrastructure/customer/repository/sequilize/customer.model";
-import CustomerRepository from "../../../infrastructure/customer/repository/sequilize/customer.repository";
 import Customer from "../../../domain/customer/entity/customer";
 import Address from "../../../domain/customer/value-object/address";
 import FindCustomerUseCase from "./find.customer.usecase";
